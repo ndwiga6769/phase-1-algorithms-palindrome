bebb07bd0737920fde4544f515089deb745e7a34
danwycliff <danwycliff6769@gmail.com>
2023-07-28 19:16:35 +0300

    palindrome

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
+function reverse(word){
+  const wordArray = word.split("")
+   const reversedWordArray = wordArray.reverse()
+   const reversedWord = reversedWordArray.join("")
+   return reversedWord;
+}
 function isPalindrome(word) {
   // Write your algorithm here
+ // make a function that returns true if a word is a 
+ // and false if a word is not (if the word is the same 
+ // from reverse)
+ //mom = mom => true
+ //abc = cba => false
+ //PSEUDOCODE
+//  reverse the string 
+//if the input is the same as the reversed string return true
+const reversedWord = reverse(word)
+if (word === reversedWord){
+  return true
+} else {
+  return false
+} 
+
+
+
+
+
+
+  function isPalindrome(string) {
+    string = string.toLowerCase().replace(/[^a-zA-Z0-9]+/g,''); // Preprocess the string to remove capitalization, spaces, and non-alphanumeric characters
+    for (let i = 0; i < string.length / 2; i++) { // Loop through half of the string
+      if (string[i] !== string[string.length - 1 - i]) { // Check if first and last string are same
+        return false; // If not, return false
+      }
+    }
+    return true; // If all characters match, return true
+  }
+  
+  // Example usage
+  console.log(isPalindrome("mom")); // Output: true
+  console.log(isPalindrome("racecar")); // Output: true
+  console.log(isPalindrome("hello")); 
 }
 
 /* 
